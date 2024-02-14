99d8d9c96433a6ba9a6b857833ea1ed0d4721246
Raushan Kumar <40159522+raushannanda@users.noreply.github.com>
2024-02-15 00:31:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
 const express = require('express')
 const app = express()
-app.use(express.static(__dirname))
+app.use(express.static(__dirname+'/template'))
 const http = require('http')
+const { hostname } = require('os')
 
 const server = http.createServer(app)
 const {Server, Socket} = require('socket.io')
@@ -14,7 +15,8 @@ const io = new Server(server,{
             'http://192.168.153.125'
         ],
         methods:['GET','POST']
-    }
+    },
+    maxHttpBufferSize:1e8
 })
 io.on('connection',(socket)=>{
     console.log('a user connected')
@@ -24,12 +26,13 @@ io.on('connection',(socket)=>{
     socket.on('chat',(msg)=>{
         socket.broadcast.emit('chat',msg)
     })
+    socket.on('upload',(file)=>{
+        socket.broadcast.emit('upload',file)
+    })
 })
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/index.html')
 })
 
-server.listen(3000,()=>{
-    console.log("listening on port 3000")
-})
+server.listen(80,'0.0.0.0')
